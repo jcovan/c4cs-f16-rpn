7be7e3de82350ca3244d3ad7ed1b6a99bd56d95d
Jessica Covan <jcovan@umich.edu>
2016-10-21 11:28:51 -0400

    add back add test case

--- test_rpn.py
+++ test_rpn.py
@@ -2,6 +2,9 @@ import unittest
 import rpn
 
 class TestBasics(unittest.TestCase):
+	def test_add(self):
+		result = rpn.calculate("2 2 +")
+		self.assertEqual(4, result)
 	def test_subtract(self):
         	result = rpn.calculate("5 3 -")
         	self.assertEqual(2, result)
